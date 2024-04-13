2ca809e3f4f0b40ddfc193deee6391438102e804
Akshay <sakshays.2000@gmail.com>
2024-04-13 14:24:05 +0200

    add basic arena grid

---
+++ arena.gd
@@ -0,0 +1,25 @@
+extends TileMap
+
+@export var _gridsize = 15
+var grid = {}
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for x in _gridsize:
+		for y in _gridsize:
+			var cell_type = randi() % 2
+			set_cell(0, Vector2i(x, y), cell_type, Vector2i(0, 0))
+			grid[Vector2i(x, y)] = {
+				"can_place": not bool(cell_type),
+				"object": null, 
+			}
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var tile = local_to_map(get_local_mouse_position())
+
+	for x in _gridsize:
+		for y in _gridsize:
+			erase_cell(1, Vector2i(x, y))
+			
+	if grid.has(tile):
+		set_cell(1, tile, 2 + int(not grid[tile]["can_place"]), Vector2i(0, 0))
