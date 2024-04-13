8d5d6fae45cd993632ae8463b7e62f273dfc73ea
Akshay <sakshays.2000@gmail.com>
2024-04-13 23:50:12 +0200

    add placeable checks

--- scenes/arena.gd
+++ scenes/arena.gd
@@ -1,21 +1,35 @@
 extends TileMap
 
-@export var _gridsize = Vector2(5, 5)
+@export var _gridsize = Vector2(13, 11)
 var grid = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#for x in _gridsize.x:
-		#for y in _gridsize.y:
-			#var cell_type = randi() % 2
-			#set_cell(0, Vector2i(x, y), cell_type, Vector2i(0, 0))
-			#grid[Vector2i(x, y)] = {
-				#"can_place": not bool(cell_type),
-				#"object": null, 
-			#}
+	for x in _gridsize.x:
+		for y in _gridsize.y:
+			var cell_type = get_cell_source_id(0, Vector2i(x, y))
+			grid[Vector2i(x, y)] = {
+				"can_place": not bool(cell_type),
+				"object": null, 
+			}
+			
 	self_modulate.a = 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var tile = local_to_map(get_local_mouse_position())
+
+
+func set_occupied(tile: Vector2i, object: Node) -> void:
+	grid[tile]["can_place"] = false
+	grid[tile]["object"] = object
+
+func is_valid_placement(tile: Vector2i) -> bool:
+	if is_cell_out_of_bounds(tile):
+		return false
+	return grid[tile]["can_place"]
+		
+	
+func is_cell_out_of_bounds(tile: Vector2i) -> bool:
+	return not get_cell_tile_data(0, tile)
+		
+#func _process(delta: float) -> void:
+	#var tile = local_to_map(get_local_mouse_position())
 
 	#for x in _gridsize:
 		#for y in _gridsize:
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -7,7 +7,12 @@ func _ready() -> void:
 	
 func _on_card_played(pos: Vector2, creature_scene: PackedScene, slot: Slot):
 	var creature = creature_scene.instantiate()
-	arena.add_child(creature)
-	creature.position = arena.map_to_local((arena.local_to_map(arena.to_local(pos))))
-	creature.slot = slot
+	var tile = arena.local_to_map(arena.to_local(pos))
+	if arena.is_valid_placement(tile):
+		arena.add_child(creature)
+		creature.position = arena.map_to_local(tile)
+		creature.slot = slot
+		arena.set_occupied(tile, creature)
+	else: 
+		slot.add_card()
 	
