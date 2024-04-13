d5fe8a1e9b7dff44d24c2d4cf73219d6c32c34e4
Akshay <sakshays.2000@gmail.com>
2024-04-13 16:57:28 +0200

    improve card drop state

--- scenes/card_ui/card_ui.gd
+++ scenes/card_ui/card_ui.gd
@@ -6,6 +6,7 @@ signal reparent_requested(which_card_ui: CardUI)
 @onready var drop_point_detector = $DropPointDetector
 @onready var state = $State
 @onready var state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
+@onready var targets: Array[Node] = []
 
 func _ready() -> void:
 	state_machine.init(self)
@@ -21,3 +22,10 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	state_machine.on_mouse_exited()
+
+func _on_drop_point_detector_area_entered(area: Area2D) -> void:
+	if not targets.has(area):
+		targets.append(area)
+
+func _on_drop_point_detector_area_exited(area: Area2D) -> void:
+	targets.erase(area)
