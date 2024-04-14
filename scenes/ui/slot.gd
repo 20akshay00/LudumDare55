335ed0938fe196e2111171dd1f671a77b78f7cf0
Akshay <sakshays.2000@gmail.com>
2024-04-15 00:14:18 +0200

    add levels

--- scenes/ui/slot.gd
+++ scenes/ui/slot.gd
@@ -2,21 +2,27 @@ class_name Slot
 extends Control
 
 @export var summon_name: String = "dragon"
-@export var card_scene: PackedScene
+var card_scene = preload("res://scenes/card_ui/card_ui.tscn")
 @onready var label := $QuantityLabel
-var quantity := 3 : 
+@export var quantity := 3 : 
 	set (value):
 		quantity = value
 		if quantity > 0:
-			label.text = str(quantity)	
+			if label:
+				label.text = str(quantity)	
 		else:
-			label.text = str("")
+			if label:
+				label.text = str("")
 	get: 
 		return quantity
 
 func _ready() -> void:
 	card_scene = preload("res://scenes/card_ui/card_ui.tscn")
-	label.text = str(quantity)
+	if quantity > 0:
+		label.text = str(quantity)
+	else:
+		label.text = ""
+		
 	label.add_theme_font_override("font", load("res://assets/art/fonts/baddie.otf"))
 	label.add_theme_font_size_override("font_size", 50)
 	#label.add_theme_color_override("font_color", "#000000")
